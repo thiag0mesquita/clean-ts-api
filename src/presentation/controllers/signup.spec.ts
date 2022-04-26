b4fe628c097011b1689b5402309849f961843656
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-04-26 14:05:25 -0300

    chore: update husky

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,7 +4,7 @@ describe('Singup Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      body: {
+body: {
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
