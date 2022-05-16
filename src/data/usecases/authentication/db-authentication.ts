638d27f3101e09e1ec573bd24789976dde3f954e
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-05-16 18:41:57 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -4,22 +4,25 @@ import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
-    private readonly hashComparer: HashComparer
-    private readonly tokenGenerator: TokenGenerator
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
 
-    constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
-      this.loadAccountByEmailRepository = loadAccountByEmailRepository
-      this.hashComparer = hashComparer
-      this.tokenGenerator = tokenGenerator
-    }
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
+  }
 
-    async auth (authentication: AuthenticationModel): Promise<string> {
-      const account = await this.loadAccountByEmailRepository.load(authentication.email)
-      if (account) {
-        await this.hashComparer.compare(authentication.password, account.password)
-        await this.tokenGenerator.generate(account.id)
+  async auth (authentication: AuthenticationModel): Promise<string> {
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account) {
+      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        return accessToken
       }
-      return null
     }
+    return null
+  }
 }
