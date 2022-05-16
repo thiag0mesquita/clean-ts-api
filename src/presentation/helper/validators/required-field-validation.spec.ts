414c154d14d8c03810398fdb0c8bb85e99ab7bd4
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-05-15 22:48:33 -0300

    test: ensure RequiredFieldValidation not return if validation succeded

--- src/presentation/helper/validators/required-field-validation.spec.ts
+++ src/presentation/helper/validators/required-field-validation.spec.ts
@@ -8,4 +8,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validation success', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })
