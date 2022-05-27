295417e2e09da34527c2ff996b8c02c0b5fa85ea
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-05-27 11:40:44 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysReposiroty

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,14 @@
+import { SurveyModel } from '../../../domain/models/surveys'
+import { LoadSurveys } from '../../../domain/usecases/load-survey'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return []
+  }
+}
