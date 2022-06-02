bdff78dd899509d68c9cf382c9c51556059b8d6d
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-06-02 15:15:40 -0300

    feat: add no cache Middleware

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const cors = (req: Request, res: Response, next: NextFunction) => {
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('acess-control-allow-origin', '*')
   res.set('acess-control-allow-headers', '*')
   res.set('acess-control-allow-methods', '*')
