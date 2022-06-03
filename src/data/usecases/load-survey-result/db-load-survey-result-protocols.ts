dfcd02c701b6c3e18f911a1c98bf37e1a6a4883f
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-06-03 19:02:54 -0300

    refactor: move LoadSurveyResult dependencies to a protocols file

---
+++ src/data/usecases/load-survey-result/db-load-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/data/protocols/db/survey-result/load-survey-result-repository'
+export * from '@/domain/models/surveys-result'
+export * from '@/domain/usecases/survey-result/load-survey-result'
