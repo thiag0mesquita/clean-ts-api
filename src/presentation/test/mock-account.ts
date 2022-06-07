d240e23e5539a582b76bc1e4c2475ec01dd5d347
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-06-07 10:49:07 -0300

    feat: add name to authentication response

--- src/presentation/test/mock-account.ts
+++ src/presentation/test/mock-account.ts
@@ -1,6 +1,7 @@
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/account/authentication'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
+import { AuthenticationModel } from '@/domain/models/authentication'
 import { AccountModel } from '@/domain/models/account'
 import { mockAccountModel } from '@/domain/test'
 
@@ -15,8 +16,11 @@ export const mockAddAccount = (): AddAccount => {
 
 export const mockAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (authentication: AuthenticationParams): Promise<string> {
-      return Promise.resolve('any_token')
+    async auth (authentication: AuthenticationParams): Promise<AuthenticationModel> {
+      return Promise.resolve({
+        accessToken: 'any_token',
+        name: 'any_name'
+      })
     }
   }
   return new AuthenticationStub()
